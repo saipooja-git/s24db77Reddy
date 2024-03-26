02ae831af39cc025e4957a9cb3d0486767be8800
saipooja-git <pallesaipooja@gmail.com>
2024-03-26 18:37:41 -0500

    final

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 Sai Pooja Reddy Palle
 S565577
 https://dashboard.render.com/
+Class Name: Ribbion
+Types: Ribbion_type,Ribbion_usesdfor,Ribbion_length
--- app.js
+++ app.js
@@ -6,6 +6,9 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var pickRouter = require('./routes/pick');
+var ribrouter = require('./routes/ribbon')
+var grouter = require('./routes/grid')
 
 var app = express();
 
@@ -21,6 +24,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/pick', pickRouter);
+app.use('/ribbon', ribrouter);
+app.use('/grid', grouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
