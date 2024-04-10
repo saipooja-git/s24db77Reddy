ae145a233c10110d05b9c3bd62ad633f58329f0d
saipooja-git <pallesaipooja@gmail.com>
2024-04-09 20:09:09 -0500

    commit

--- controllers/ribbon.js
+++ controllers/ribbon.js
@@ -1,59 +1,105 @@
 var ribbon = require('../models/ribbon');
 // List of all ribbon
-// List of all Costumes
-exports.ribbon_list = async function(req, res) {
-    try{
-    theribbon = await ribbon.find();
-    res.send(theribbon);
+// List of all ribbons
+exports.ribbon_list = async function (req, res) {
+    try {
+        theribbon = await ribbon.find();
+        res.send(theribbon);
     }
-    catch(err){
-    res.status(500);
-    res.send(`{"error": ${err}}`);
+    catch (err) {
+        res.status(500);
+        res.send(`{"error": ${err}}`);
     }
-    };
+};
 // VIEWS
 // Handle a show all view
-exports.ribbon_view_all_Page = async function(req, res) {
-    try{
-    theribbons = await ribbon.find();
-    res.render('ribbon', { title: 'ribbon Search Results', results: theribbons });
+exports.ribbon_view_all_Page = async function (req, res) {
+    try {
+        theribbons = await ribbon.find();
+        res.render('ribbon', { title: 'ribbon Search Results', results: theribbons });
     }
-    catch(err){
-    res.status(500);
-    res.send(`{"error": ${err}}`);
+    catch (err) {
+        res.status(500);
+        res.send(`{"error": ${err}}`);
     }
-    };    
-        
-// for a specific ribbon.
-exports.ribbon_detail = function(req, res) {
-res.send('NOT IMPLEMENTED: ribbon detail: ' + req.params.id);
 };
+
+// // for a specific ribbon.
+// exports.ribbon_detail = function(req, res) {
+// res.send('NOT IMPLEMENTED: ribbon detail: ' + req.params.id);
+// };
+//for a specific ribbon.
+exports.ribbon_detail = async function (req, res) {
+    console.log("detail" + req.params.id)
+    try {
+        result = await ribbon.findById(req.params.id)
+        res.send(result)
+    } catch (error) {
+        res.status(500)
+        res.send(`{"error": document for id ${req.params.id} not found`);
+    }
+};
+
+
 // Handle ribbon create on POST.
-exports.ribbon_create_post = async function(req, res) {
+exports.ribbon_create_post = async function (req, res) {
     console.log(req.body)
     let document = new ribbon();
     // We are looking for a body, since POST does not have query parameters.
     // Even though bodies can be in many different formats, we will be picky
     // and require that it be a json object
-    // {"costume_type":"goat", "cost":12, "size":"large"}
+    // {"ribbon_type":"goat", "cost":12, "size":"large"}
     document.ribbon_type = req.body.ribbon_type;
     document.ribbon_usedfor = req.body.ribbon_usedfor;
     document.ribbon_length = req.body.ribbon_length;
-    try{
-    let result = await document.save();
-    res.send(result);
+    try {
+        let result = await document.save();
+        res.send(result);
     }
-    catch(err){
-    res.status(500);
-    res.send(`{"error": ${err}}`);
+    catch (err) {
+        res.status(500);
+        res.send(`{"error": ${err}}`);
     }
-    };
-    
-// Handle ribbon delete from on DELETE.
-exports.ribbon_delete = function(req, res) {
-res.send('NOT IMPLEMENTED: ribbon delete DELETE ' + req.params.id);
 };
+
+// Handle ribbon delete from on DELETE.
+// exports.ribbon_delete = function (req, res) {
+//     res.send('NOT IMPLEMENTED: ribbon delete DELETE ' + req.params.id);
+// };
+//Handle Costume delete on DELETE.
+exports.ribbon_delete = async function(req, res) {
+console.log("delete " + req.params.id)
+try {
+result = await ribbon.findByIdAndDelete( req.params.id)
+console.log("Removed " + result)
+res.send(result)
+} catch (err) {
+res.status(500)
+res.send(`{"error": Error deleting ${err}}`);
+}
+};
+
+// // Handle ribbon update form on PUT.
+// exports.ribbon_update_put = function (req, res) {
+//     res.send('NOT IMPLEMENTED: ribbon update PUT' + req.params.id);
+// };
 // Handle ribbon update form on PUT.
-exports.ribbon_update_put = function(req, res) {
-res.send('NOT IMPLEMENTED: ribbon update PUT' + req.params.id);
+exports.ribbon_update_put = async function (req, res) {
+    console.log(`update on id ${req.params.id} with body
+${JSON.stringify(req.body)}`)
+    try {
+        let toUpdate = await ribbon.findById(req.params.id)
+        // Do updates of properties
+        if (req.body.ribbon_type)
+            toUpdate.ribbon_type = req.body.ribbon_type;
+        if (req.body.ribbon_usedfor) toUpdate.ribbon_usedfor = req.body.ribbon_usedfor;
+        if (req.body.ribbon_length) toUpdate.ribbon_length = req.body.ribbon_length;
+        let result = await toUpdate.save();
+        console.log("Sucess " + result)
+        res.send(result)
+    } catch (err) {
+        res.status(500)
+        res.send(`{"error": ${err}: Update for id ${req.params.id}
+failed`);
+    }
 };
--- routes/resource.js
+++ routes/resource.js
@@ -18,3 +18,14 @@ router.get('/ribbon/:id', ribbon_controller.ribbon_detail);
 // GET request for list of all ribbon items.
 router.get('/ribbon', ribbon_controller.ribbon_list);
 module.exports = router;
+// //for a specific Costume.
+// exports.costume_detail = async function(req, res) {
+// console.log("detail" + req.params.id)
+// try {
+// result = await Costume.findById( req.params.id)
+// res.send(result)
+// } catch (error) {
+// res.status(500)
+// res.send(`{"error": document for id ${req.params.id} not found`);
+// }
+// };
